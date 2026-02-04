08f51a9e155624c274b9d059c22a689a8f97fee6
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-04 09:15:44 +0700

    week01: wrong variable name, wrote score as name so name was doubled

--- src/oop_140764_JeremiahEJ/week01/GradingSystem.kt
+++ src/oop_140764_JeremiahEJ/week01/GradingSystem.kt
@@ -4,7 +4,9 @@ package oop_140764_JeremiahEJ.week01
 
 fun main() {
     var name: String = "John Thor"
-    var name: Int = 80
+    var nilai: Int = 80
 
     println("Nama: $name, Nilai: $score")
+
+    val grade = when (score)
 }
