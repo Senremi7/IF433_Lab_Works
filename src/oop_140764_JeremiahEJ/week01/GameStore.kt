1f49fd01906150d80a189a2d1b8bb5610735e582
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-04 10:58:01 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_140764_JeremiahEJ/week01/GameStore.kt
+++ src/oop_140764_JeremiahEJ/week01/GameStore.kt
@@ -1,4 +1,12 @@
 package oop_140764_JeremiahEJ.week01
 
 class GameStore {
+    val gameTitle: String = "R.E.P.O"
+    val price: Int = 84500
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000)
+        (price * 0.8).toInt()
+    else
+        (price * 0.9).toInt()
