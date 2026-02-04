6735f2b8d9b70573eafc45948a726b9a4c53e9fb
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-04 09:38:32 +0700

    week01: refactor to use val and string templates

--- src/oop_140764_JeremiahEJ/week01/GradingSystem.main.kts
+++ src/oop_140764_JeremiahEJ/week01/GradingSystem.main.kts
@@ -1,2 +1,11 @@
 #!/usr/bin/env kotlin
 
+package oop_140764_JeremiahEJ.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: $name, Nilai: $score")
+
+}
