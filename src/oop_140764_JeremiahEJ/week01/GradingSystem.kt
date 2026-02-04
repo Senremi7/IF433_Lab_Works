f1225444da745213ac3f8e51a5b4f4d3dde42350
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-04 09:12:50 +0700

    week01: initial setup and procedural code

---
+++ src/oop_140764_JeremiahEJ/week01/GradingSystem.kt
@@ -0,0 +1,10 @@
+#!/usr/bin/env kotlin
+
+package oop_140764_JeremiahEJ.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var name: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
