15dd72a469b09a20b3346bf7aaabd0cf7513fcd7
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-18 10:38:19 +0700

    week03: test all encapsulation logic in main

--- src/oop_140764_JeremiahEJ/week03/Main.kt
+++ src/oop_140764_JeremiahEJ/week03/Main.kt
@@ -2,6 +2,17 @@ package oop_140764_JeremiahEJ.week03
 
 fun main() {
     val e = Employee("Budawg")
-    println("=== Testing error ===")
-    e.salary = 5000
+
+    // Validation
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    // encapsulation
+    e.increasePerformance()
+    // error because private property
+    // e.performanceRating = 5
+
+    // Computed property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
