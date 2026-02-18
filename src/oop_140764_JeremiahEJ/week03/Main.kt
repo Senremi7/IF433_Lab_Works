ab61cae81cdaae9754cd3ccaae2a30594d90e25b
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-18 11:26:38 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_140764_JeremiahEJ/week03/Main.kt
+++ src/oop_140764_JeremiahEJ/week03/Main.kt
@@ -11,11 +11,28 @@ fun main() {
 
     println("Pajak yang harus dibayar: ${e.tax}")
 
-    val w = Weapon("Excalibur", 600)
+    val w = Weapon("Axe of Peace", 600)
 
-    w.damage = -50
-    w.damage = 9999
-
+    println("Weapon : ${w.name}")
     println("Damage: ${w.damage}")
     println("Tier: ${w.tier}")
+
+    w.damage = -50
+    println("Weapon : ${w.name}")
+    println("Damage: ${w.damage}")
+    println("Tier: ${w.tier}")
+
+    w.damage = 9999
+    println("Weapon : ${w.name}")
+    println("Damage: ${w.damage}")
+    println("Tier: ${w.tier}")
+
+    val player = Player("Technoblade")
+
+    //player.xp
+    //Kotlin: Cannot access 'var xp: Int': it is private in 'oop_140764_JeremiahEJ.week03.Player'.
+    player.addXp(50)
+    println("Current level: ${player.level}")
+    player.addXp(60)
+    println("Current level: ${player.level}")
 }
