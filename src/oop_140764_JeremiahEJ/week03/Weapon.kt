367bafed8e6481ee2195740fc2475f6e8691e08d
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-18 10:46:50 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_140764_JeremiahEJ/week03/Weapon.kt
@@ -0,0 +1,30 @@
+package oop_140764_JeremiahEJ.week03
+
+class Weapon(
+    val name: String,
+    damage: Int
+){
+    var damage: Int = damage
+        set(value) {
+            if (value < 0){
+                println("ERROR: Damage tidak boleh negatif! Di-set ke 0.")
+                field = value
+            } else if (value > 1000) {
+                println("Damage terlalu overpowered woi! Capped at 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() {
+            if (damage > 800){
+                return "Legendary"
+            } else if (damage > 500){
+                return "Epic"
+            } else {
+                return "Common"
+            }
+        }
+}
