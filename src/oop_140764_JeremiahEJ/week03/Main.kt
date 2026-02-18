5549431a4331d2b48a07954520c86d3f243555cc
Senremi7 <jeremiah.remi7@gmail.com>
2026-02-18 10:38:46 +0700

    week03: cleanup code

--- src/oop_140764_JeremiahEJ/week03/Main.kt
+++ src/oop_140764_JeremiahEJ/week03/Main.kt
@@ -3,16 +3,11 @@ package oop_140764_JeremiahEJ.week03
 fun main() {
     val e = Employee("Budawg")
 
-    // Validation
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // encapsulation
     e.increasePerformance()
-    // error because private property
-    // e.performanceRating = 5
 
-    // Computed property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
