98fa46a12e45a3e4c2e4fb423dc7fc0182f1c18b
Senremi7 <jeremiah.remi7@gmail.com>
2026-03-11 10:18:24 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_140764_JeremiahEJ/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_140764_JeremiahEJ.week06
+
+interface Camera{
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone{
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
