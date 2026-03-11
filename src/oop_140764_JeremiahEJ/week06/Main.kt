bec1d2a8733b19303a99e22b1a614ac8fa85983c
Senremi7 <jeremiah.remi7@gmail.com>
2026-03-11 11:08:09 +0700

    week06: (task) test smart home system logic in main

--- src/oop_140764_JeremiahEJ/week06/Main.kt
+++ src/oop_140764_JeremiahEJ/week06/Main.kt
@@ -6,20 +6,35 @@ fun processCheckout(method: PaymentMethod, amount: Double){
 }
 
 fun main(){
-//    val myWatch = Smartwatch()
-//    myWatch.showTime()
-//
-//    val myPhone = Smartphone()
-//    myPhone.turnOn()
-//
-//    val pay1 = Gopay()
-//    val pay2 = CreditCard()
-//
-//    println("\n=== TESTING CHECKOUT ===")
-//    processCheckout(pay1, 50000.0)
-//    processCheckout(pay2, 150000.0)
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(pay1, 50000.0)
+    processCheckout(pay2, 150000.0)
 
     val lamp = SmartLamp("Lamp01", "Ruang Tamu")
     val speaker = SmartSpeaker("Speaker01", "Google Nest Dapur")
     val cctv = SmartCCTV("CCTV01", "Ezviz Garasi")
+
+    println("=== TESTING turnON FUNCTION ON SMART DEVICEd ===")
+    lamp.turnOn()
+    speaker.turnOn()
+    cctv.turnOn()
+
+    println("\n=== TESTING SMART HOME HUB ===")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
