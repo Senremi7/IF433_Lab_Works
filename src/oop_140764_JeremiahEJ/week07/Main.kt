1427870330c164436135cd13303126ecdb3af45e
Senremi7 <jeremiah.remi7@gmail.com>
2026-03-25 10:21:08 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_140764_JeremiahEJ/week07/Main.kt
+++ src/oop_140764_JeremiahEJ/week07/Main.kt
@@ -1,6 +1,6 @@
 package oop_140764_JeremiahEJ.week07
 
 fun main(){
-    val client = NetworkClient("https://api.umn.ac.id"/)
+    val client = NetworkClient("https://api.umn.ac.id")
     client.connect()
 }
