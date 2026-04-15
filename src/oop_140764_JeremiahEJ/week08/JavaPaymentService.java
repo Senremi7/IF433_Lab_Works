7cadd58ce03b7838a3ffc52019d9332638aaa936
Senremi7 <jeremiah.remi7@gmail.com>
2026-04-15 19:16:16 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_140764_JeremiahEJ/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_140764_JeremiahEJ.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productID) {
+        return "TRS-" + productID + "-SUCCESS";
+    }
+}
