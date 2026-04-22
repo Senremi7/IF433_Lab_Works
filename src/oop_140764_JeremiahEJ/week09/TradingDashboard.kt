a2b089a2f97930c443a3c3a562399f1ab6062a2b
Senremi7 <jeremiah.remi7@gmail.com>
2026-04-22 10:41:50 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_140764_JeremiahEJ/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_140764_JeremiahEJ.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("XAUUSD", "LONG", 10, 15.5, "OPEN"),
+        TradeLog("EURUSD", "SHORT", 5, -8.2, "CLOSED"),
+        TradeLog("BTCUSD", "LONG", 20, 25.0, "OPEN"),
+        TradeLog("USDJPY", "SHORT", 15, -12.3, "CLOSED"),
+        TradeLog("BBCA", "LONG", 3, 3.2, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 13, 15.0, "OPEN")
+    )
+}
