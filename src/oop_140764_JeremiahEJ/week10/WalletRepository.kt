374f11db69ade145c06029b157c71669a4447006
Senremi7 <jeremiah.remi7@gmail.com>
2026-04-29 10:53:26 +0700

    week10: (task) implement search logic in repository

--- src/oop_140764_JeremiahEJ/week10/WalletRepository.kt
+++ src/oop_140764_JeremiahEJ/week10/WalletRepository.kt
@@ -3,7 +3,12 @@ package oop_140764_JeremiahEJ.week10
 class WalletRepository<T> {
     private val items = mutableListOf<T>()
 
-    fun addItem(item: T) = items.add(item)
+    fun add(item: T) = items.add(item)
 
-    fun getAllItems(): List<T> = return items
+    fun getAll(): List<T> = items
+
+    fun pencarian(item: T)
+    : List<T> {
+        return items.filter { it == item }
+    }
 }
