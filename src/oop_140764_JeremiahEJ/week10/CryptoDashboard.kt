0d8a146dbb2c43d60f2371a9d58f9e5de635adf1
Senremi7 <jeremiah.remi7@gmail.com>
2026-04-29 11:08:07 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_140764_JeremiahEJ/week10/CryptoDashboard.kt
+++ src/oop_140764_JeremiahEJ/week10/CryptoDashboard.kt
@@ -1,6 +1,7 @@
 package oop_140764_JeremiahEJ.week10
 
 fun main() {
+    println("=== CRYPTO DASHBOARD ===")
     val coinRepo = WalletRepository<Coin>()
     coinRepo.add(Coin("BTC", 65000.0))
     coinRepo.add(Coin("ETH", 3700.0))
@@ -10,8 +11,12 @@ fun main() {
 
     response.data.forEach { println("Coin: ${it.name} \nBalace: ${it.balance}") }
 
+    println("\n=== TRANSACTIONS ===")
     val txRepo = WalletRepository<Transaction>()
     txRepo.add(Transaction("A1", 1.0))
     txRepo.add(Transaction("A2", 0.5))
     txRepo.add(Transaction("A3", 2.0))
+
+    val txResponse = ApiResponse("200 OK", txRepo.getAll())
+    txResponse.data.forEach { println("Transaction: ${it.id} \nAmount: ${it.amount}") }
 }
