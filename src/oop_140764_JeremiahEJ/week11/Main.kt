5a0efe47102fbe20ca3e8edee3008d0b08207d45
Senremi7 <jeremiah.remi7@gmail.com>
2026-05-06 10:20:04 +0700

    week11: implement run function for object computation

--- src/oop_140764_JeremiahEJ/week11/Main.kt
+++ src/oop_140764_JeremiahEJ/week11/Main.kt
@@ -7,4 +7,11 @@ fun main (){
 
     val text: String? = null
     println("Apakah null/empty? ${text.isNullOrEmpty()}")
+
+    println("\n=== TEST RUN FUCTION===")
+    val result = "Kotlin".run{
+        println("Memproses kata: ${this}")
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
 }
