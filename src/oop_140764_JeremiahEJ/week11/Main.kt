7fcceb7088dd681ea7929618204ed76efb8f2c55
Senremi7 <jeremiah.remi7@gmail.com>
2026-05-06 10:16:28 +0700

    week11: test extension functions in main

---
+++ src/oop_140764_JeremiahEJ/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_140764_JeremiahEJ.week11
+
+fun main (){
+    println("=== TEST EXTENSION FUNCTION==")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmpty()}")
+}gi
