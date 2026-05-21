510e0809eeae00263a3f5088c6d14fb22b796221
Senremi7 <jeremiah.remi7@gmail.com>
2026-05-21 18:33:12 +0700

    week13: create Student model

---
+++ src/oop_140764_JeremiahEJ/week13/StudentSerialization.kt
@@ -0,0 +1,9 @@
+package oop_140764_JeremiahEJ.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
