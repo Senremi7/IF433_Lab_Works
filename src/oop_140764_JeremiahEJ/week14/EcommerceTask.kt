de3b657918c28d43dee12ff1c64ce641b20f67d1
Senremi7 <jeremiah.remi7@gmail.com>
2026-05-27 00:24:10 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

---
+++ src/oop_140764_JeremiahEJ/week14/EcommerceTask.kt
@@ -0,0 +1,58 @@
+package oop_140764_JeremiahEJ.week14
+import java.io.File
+
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    private val file = File("orders.csv")
+
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(message: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(message: String) {
+        println("Email terkirim: Pesanan Anda telah dikonfirmasi! Detail: $message")
+    }
+}
+
+class SafeOrderProcessor(val repo: OrderRepository, val notifier: NotificationService){
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String){
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification("Pesanan $itemName \n Harga final: $finalPrice")
+    }
+}
+//
+//class BadOrderProcessor {
+//    private val file = File("orders.csv")
+//
+//    fun processOrder(itemName: String, basePrice: Double, customerType: String){
+//        val finalPrice = when (customerType) {
+//            "REGULAR" -> basePrice
+//            "VIP" -> basePrice * 0.9
+//            else -> basePrice
+//        }
+//
+//        println("Memproses pesanan $itemName seharga $finalPrice")
+//
+//        file.appendText("$itemName,$finalPrice,$customerType\n")
+//
+//        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+//    }
+//}
