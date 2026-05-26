73e6d7c7e023a7721a69af64fbf1fb790b2ee1e6
Senremi7 <jeremiah.remi7@gmail.com>
2026-05-26 22:56:28 +0700

    week14: extract UserValidator and UserRepository

---
+++ src/oop_140764_JeremiahEJ/week14/SrpRefactored.kt
@@ -0,0 +1,11 @@
+package oop_140764_JeremiahEJ.week14
+
+class UserValidator {
+    fun validateUser(user: User): Boolean = user.email.contains("@") && user.age >= 18
+}
+
+class UserRepository {
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
